ccdc4031f9be8ac506c13b685d03782432cc75ea
Kamiran Ibrahim <kamiran.ibrahim@gmail.com>
2020-08-14 13:36:01 -0500

    add sharkTank and Graveyard

---
+++ src/components/GraveStone/GraveStone.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class GraveStone extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>access to the Component</h4>
+      </div>
+    );
+  }
+}
+
+export default GraveStone;
---
+++ src/components/Graveyard/Graveyard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Graveyards extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>access to the Component</h4>
+      </div>
+    );
+  }
+}
+
+export default Graveyards;
